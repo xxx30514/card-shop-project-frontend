e8c17e531037968a00c522079ffbdb71b0cf2eb5
xxx30514 <xxx30514@gmail.com>
2024-08-28 21:32:56 +0800

    rename test

--- src/pages/LifecycleHooks.vue
+++ src/pages/LifecycleHooks.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>{{ sum }}</h1>
   <button @click="add">+1</button>
+  <button @click="add">+1</button>
 </template>
 <script setup lang="ts">
 import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from 'vue';
